ecdba850efa5a0bf633881ec229dfb4e0f657722
Erdem Özkök <51801996+erdemozk@users.noreply.github.com>
2020-08-22 11:16:32 +0300

    Animation-Test

--- src/views/ParallaxList.js
+++ src/views/ParallaxList.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   View,
   FlatList,
+  ScrollView,
+  Animated
 } from 'react-native';
 import Item from './Item';
 
+//x.nativeEvent.contentOffset.y
+
 const ParallaxList = (props) => {
+    const scrollA = useRef(new Animated.Value(0)).current;
     return (
         <View>
             <FlatList
                 data={props.data}
-                renderItem={({ item }) => (Item(item))}>
+                extraData={props.data}
+                renderItem={({ item }) => (Item(item))}
+                renderScrollComponent={(props) => <ScrollView {...props} onScroll={(x) => {
+                    Animated.event(
+                        [{nativeEvent: {contentOffset: {y: scrollA}}}],
+                        {useNativeDriver: true}
+                    )
+                }}/>}>
             </FlatList>
         </View>
     );
